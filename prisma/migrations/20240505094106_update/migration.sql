4f9df54942310623266bd1800e7131f103f83708
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-05 15:16:58 +0530

    update schema

---
+++ prisma/migrations/20240505094106_update/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income'
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income',
+    "category" TEXT NOT NULL,
+    "categoryIcon" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "MonthHistory" (
+    "userId" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "YearHistory" (
+    "userId" TEXT NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_type_userId_key" ON "Category"("name", "type", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MonthHistory_day_month_year_userId_key" ON "MonthHistory"("day", "month", "year", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "YearHistory_month_year_userId_key" ON "YearHistory"("month", "year", "userId");

caf5dca288429d028da14525d43c0e16b071bd7b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-06 04:03:49 +0530

    zod validation for updating currency schema

---
+++ schame/userSettings.ts
@@ -0,0 +1,12 @@
+import { Currencies } from "@/lib/currencies";
+import * as z from "zod";
+
+export const UpdateUserCurrencySchame = z.object({
+    currency: z.custom((value) => {
+        const found = Currencies.some((c) => c.value === value)
+        if (!found) {
+            throw new Error(`invalid currency: ${value}`)
+        }
+        return value;
+    })
+}) 

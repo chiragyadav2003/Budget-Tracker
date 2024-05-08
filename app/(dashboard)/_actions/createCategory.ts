f2e01730ce15559434c6ed9cd2b9b25d743dc328
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-08 07:10:11 +0530

    server action to create a new category

---
+++ app/(dashboard)/_actions/createCategory.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import prisma from "@/lib/db";
+import { CreateCategorySchema, CreateCategorySchemaType } from "@/schema/categories";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function CreateCategory(form: CreateCategorySchemaType) {
+
+    const parsedBody = CreateCategorySchema.safeParse(form);
+    if (!parsedBody.success) {
+        throw new Error("bad request");
+    }
+
+    const user = await currentUser();
+    if (!user) {
+        redirect("/sign-in");
+    }
+
+    const { name, icon, type } = parsedBody.data;
+
+    let res;
+    try {
+        res = await prisma.category.create({
+            data: {
+                userId: user.id,
+                name,
+                icon,
+                type
+            }
+        });
+    } catch (error) {
+        console.error("Error creating category:", error);
+        throw new Error("db updation error");
+    }
+
+    return res;
+}

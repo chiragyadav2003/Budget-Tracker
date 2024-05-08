dd9729a6cd4f735767ae017d70912d8b0b4fc05e
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-08 07:15:03 +0530

    finish with category picker

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,63 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
+
+
+async function DashboardPage() {
+
+  const user = await currentUser()
+  if (!user) {
+    redirect("sign-in");
+  }
+
+  const userSettings = await prisma.userSettings.findFirst({
+    where: {
+      userId: user.id
+    }
+  })
+  if (!userSettings) {
+    redirect("/wizard");
+  }
 
 
   return (
-    <div>Budget Tracker</div>
+    <div className=" h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+
+          <p className=" text-3xl font-bold">
+            Hello, {user.firstName}! 👋
+          </p>
+
+          <div className=" flex items-center gap-3">
+            <CreateTransactionDialog
+              type={"income"}
+              trigger={
+                <Button variant={"outline"}
+                  className=" border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700"
+                >
+                  New income 🤑
+                </Button>
+              }
+            />
+
+            <CreateTransactionDialog
+              type={"expense"}
+              trigger={
+                <Button variant={"outline"}
+                  className=" border-rose-500 bg-rose-950 text-white hover:bg-rose-700"
+                >
+                  New expense 🤬
+                </Button>
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default DashboardPage;

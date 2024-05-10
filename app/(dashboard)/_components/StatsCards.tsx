779aa85a221936bdc7a680b10d9f794138a009d9
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-10 10:44:25 +0530

    little ui fix

--- app/(dashboard)/_components/StatsCards.tsx
+++ app/(dashboard)/_components/StatsCards.tsx
@@ -89,7 +89,7 @@ function StatsCard({ formatter, value, title, icon }: {
     return (
         <Card className="flex h-24 w-full items-center gap-2 p-4">
             {icon}
-            <div className="flex flex-col items-center gap-0">
+            <div className="flex flex-col items-start gap-0">
                 <p className=" text-muted-foreground">{title}</p>
                 <CountUp
                     preserveValue

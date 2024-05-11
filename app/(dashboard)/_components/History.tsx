5f28280b546e0efd30c6106428831d4a7c5ce4b0
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-11 17:10:15 +0530

    added chart

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { GetFormatterForCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import HistoryPeriodSelector from "./HistoryPeriodSelector";
 import { useQuery } from "@tanstack/react-query";
 import { GetHistoryDataReturnType } from "@/app/api/history-data/route";
@@ -19,6 +19,8 @@ import {
     XAxis,
     YAxis,
 } from "recharts";
+import { cn } from "@/lib/utils";
+import CountUp from "react-countup";
 
 
 
@@ -73,9 +75,6 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                 </CardHeader>
                 <CardContent>
                     <SkeletonWrapper isLoading={historyDataQuery.isFetching}>
-                        {dataAvailable && (
-                            <div>CHART</div>
-                        )}
                         {
                             !dataAvailable && (
                                 <Card className="flex h-[300px] flex-col items-center justify-center bg-background">
@@ -86,6 +85,124 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                                 </Card>
                             )
                         }
+
+                        {
+                            dataAvailable && (
+                                <ResponsiveContainer
+                                    width={"100%"}
+                                    height={300}
+                                >
+                                    <BarChart
+                                        height={300}
+                                        data={historyDataQuery.data}
+                                        barCategoryGap={5}
+                                    >
+                                        {/* define linear gradient for chart */}
+                                        <defs>
+                                            <linearGradient
+                                                id="incomeBar"
+                                                x1={"0"}
+                                                y1={"0"}
+                                                x2={"0"}
+                                                y2={"1"}
+                                            >
+                                                <stop
+                                                    offset={"0"}
+                                                    stopColor="#10b981"
+                                                    stopOpacity={"1"}
+                                                />
+                                                <stop
+                                                    offset={"1"}
+                                                    stopColor="#10b981"
+                                                    stopOpacity={"0"}
+                                                />
+                                            </linearGradient>
+                                            <linearGradient
+                                                id="expenseBar"
+                                                x1={"0"}
+                                                y1={"0"}
+                                                x2={"0"}
+                                                y2={"1"}
+                                            >
+                                                <stop
+                                                    offset={"0"}
+                                                    stopColor="#ef4444"
+                                                    stopOpacity={"1"}
+                                                />
+                                                <stop
+                                                    offset={"1"}
+                                                    stopColor="#ef4444"
+                                                    stopOpacity={"0"}
+                                                />
+                                            </linearGradient>
+                                        </defs>
+
+                                        <CartesianGrid
+                                            strokeDasharray={"5 5"}
+                                            strokeOpacity={"0.2"}
+                                            vertical={false}
+                                        />
+
+                                        <XAxis
+                                            stroke="#888888"
+                                            fontSize={12}
+                                            tickLine={false}
+                                            axisLine={false}
+                                            padding={{ left: 5, right: 5 }}
+                                            dataKey={(data) => {
+                                                const { year, month, day } = data;
+                                                const date = new Date(year, month, day || 1);
+                                                //return "month" for x-axis data if timeframe is month else return days
+                                                if (timeframe === "year") {
+                                                    return date.toLocaleString("default", {
+                                                        month: "long"
+                                                    });
+                                                }
+
+                                                return date.toLocaleString("default", {
+                                                    day: "2-digit"
+                                                });
+                                            }}
+                                        />
+
+                                        <YAxis
+                                            stroke="#888888"
+                                            fontSize={12}
+                                            tickLine={false}
+                                            axisLine={false}
+                                        />
+
+                                        <Bar
+                                            dataKey={"income"}
+                                            label="Income"
+                                            fill="url(#incomeBar)"
+                                            radius={4}
+                                            className=" cursor-pointer"
+                                        />
+
+                                        <Bar
+                                            dataKey={"expense"}
+                                            label="Expense"
+                                            fill="url(#expenseBar)"
+                                            radius={4}
+                                            className=" cursor-pointer"
+                                        />
+
+                                        <Tooltip
+                                            cursor={{ opacity: 0.1 }}
+                                            content={props => (
+                                                <CustomToolTip
+                                                    formatter={formatter}
+                                                    {...props}
+                                                />
+                                            )}
+                                        />
+
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            )
+                        }
+
                     </SkeletonWrapper>
                 </CardContent>
             </Card>
@@ -94,3 +211,72 @@ function History({ userSettings }: { userSettings: UserSettings }) {
 }
 
 export default History;
+
+function CustomToolTip({ active, payload, formatter }: any) {
+    if (!active || !payload || payload.length === 0) return null;
+
+    const data = payload[0].payload;
+    const { expense, income } = data;
+    return (
+        <div className=" min-w-[300px] rounded border bg-background p-4">
+            <TooltipRow
+                formatter={formatter}
+                label="Expense"
+                value={expense}
+                bgColor="bg-red-500"
+                textColor="text-red-500"
+            />
+
+            <TooltipRow
+                formatter={formatter}
+                label="Income"
+                value={income}
+                bgColor="bg-emerald-500"
+                textColor="text-emerald-500"
+            />
+
+            <TooltipRow
+                formatter={formatter}
+                label="Balance"
+                value={income - expense}
+                bgColor="bg-gray-100"
+                textColor="text-foreground"
+            />
+
+        </div>
+    )
+}
+
+function TooltipRow({
+    label, value, bgColor, textColor, formatter
+}: {
+    label: string;
+    value: number;
+    bgColor: string;
+    textColor: string;
+    formatter: Intl.NumberFormat;
+}) {
+
+    const forattingFn = useCallback((value: number) => {
+        return formatter.format(value)
+    }, [formatter])
+
+    return (
+        <div className="flex items-center gap-2">
+            <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+            <div className="flex w-full justify-between">
+                <p className="text-sm text-muted-foreground">{label}</p>
+                <div className={cn("text-sm font-bold", textColor)}>
+                    <CountUp
+                        duration={0.5}
+                        preserveValue
+                        end={value}
+                        decimals={0}
+                        formattingFn={forattingFn}
+                        className="text-sm"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}

7f9d7e372f913a4858912a117da9e42755bdc04b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-13 23:10:30 +0530

    final upgrade

--- prisma/migrations/20240505092731_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "UserSetting" (
-    "userId" TEXT NOT NULL PRIMARY KEY,
-    "currency" TEXT NOT NULL
-);
--- prisma/migrations/20240513173809_vercel/migration.sql
+++ prisma/migrations/20240513173809_vercel/migration.sql
@@ -1,6 +1,14 @@
 -- CreateTable
+CREATE TABLE "UserSettings" (
+    "userId" TEXT NOT NULL,
+    "currency" TEXT NOT NULL,
+
+    CONSTRAINT "UserSettings_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
 CREATE TABLE "Category" (
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "icon" TEXT NOT NULL,
@@ -9,16 +17,18 @@ CREATE TABLE "Category" (
 
 -- CreateTable
 CREATE TABLE "Transaction" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "amount" REAL NOT NULL,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" DOUBLE PRECISION NOT NULL,
     "description" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "userId" TEXT NOT NULL,
     "type" TEXT NOT NULL DEFAULT 'income',
     "category" TEXT NOT NULL,
-    "categoryIcon" TEXT NOT NULL
+    "categoryIcon" TEXT NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -27,8 +37,8 @@ CREATE TABLE "MonthHistory" (
     "day" INTEGER NOT NULL,
     "month" INTEGER NOT NULL,
     "year" INTEGER NOT NULL,
-    "income" REAL NOT NULL,
-    "expense" REAL NOT NULL
+    "income" DOUBLE PRECISION NOT NULL,
+    "expense" DOUBLE PRECISION NOT NULL
 );
 
 -- CreateTable
@@ -36,8 +46,8 @@ CREATE TABLE "YearHistory" (
     "userId" TEXT NOT NULL,
     "month" INTEGER NOT NULL,
     "year" INTEGER NOT NULL,
-    "income" REAL NOT NULL,
-    "expense" REAL NOT NULL
+    "income" DOUBLE PRECISION NOT NULL,
+    "expense" DOUBLE PRECISION NOT NULL
 );
 
 -- CreateIndex
